c2d102d79fcb1e0a1f10d7f744ada47da21c7150
zhusw <sink50@foxmail.com>
2023-05-02 14:52:58 +0800

    队列，入队，出队，搬移，扩容

--- lib/src/main/java/com/practice/lib/queue/Queue.kt
+++ lib/src/main/java/com/practice/lib/queue/Queue.kt
@@ -1,52 +1,90 @@
 package com.practice.lib.queue
 
-import com.practice.lib.print
-
-
+// 先进先出 FIFO
+//需要考虑 多次 入队出队后，tail 移动到了尾部时
+//选择整体搬移队列头部，或扩容
 fun main() {
-    val aq = ArrayQueue()
-    aq.enqueue("1")
-    aq.enqueue("2")
-    aq.enqueue("3")
-    "size:${aq.size()}".print()
-    val v = aq.dequeue()
-    "v:$v  size:${aq.size()}".print()
+    val aq = MyStringQueue(5)
+    aq.enqueue("A")
+    aq.enqueue("B")
+    aq.enqueue("C")
+    aq.enqueue("D")
+    aq.enqueue("E")
+    aq.printElement()
 
 }
 
 /**
  * 搞个队列
  */
-class ArrayQueue {
+class MyStringQueue(private val initSize: Int) {
+    private val TEMP = ""
+    var size: Int
+        private set
 
-    companion object {
-        private const val MAC_COUNT = 10
+    init {
+        size = initSize
     }
 
-    private val items = Array(MAC_COUNT) { "" }
+    private var items = Array(size) { "" }
 
     private var head = 0
     private var tail = 0
 
-
-    fun enqueue(e: String): Boolean {
-        if (tail == MAC_COUNT) return false
-        items[tail] = e
+    fun enqueue(element: String): Boolean {
+        if (tail == size) {
+            if (head >= (size / 2)) {
+                //如果空余 空间大于 size / 2 进行 搬移
+                for (i in head until tail) {
+                    items[i - head] = items[i]
+                }
+                tail -= head // or tail = count()+1
+                head = 0
+            } else {
+                //进行扩容，size x 2
+                val newSize = size * 2
+                val newArray = Array(newSize) { "" }
+                for (i in head until tail) {
+                    val item = items[i]
+                    newArray[i - head] = item
+                }
+                size = newSize
+                items = newArray
+                tail -= head
+                head = 0
+            }
+        }
+        items[tail] = element
         ++tail
         return true
     }
 
     fun dequeue(): String {
-        if (tail != 0 && head != tail) {
-            val e = items[head]
-            ++head
-            return e
-        }
-        throw IndexOutOfBoundsException("dequeue fail,queue is empty")
+        //过滤无元素
+        if (head != tail) throw ArrayIndexOutOfBoundsException(
+            "element:${count()}" +
+                    ",head:$head,tail:$tail"
+        )
+        val leftElement = items[head]
+        // 消除引用，避免泄漏
+        items[head] = TEMP
+        ++head
+        return leftElement
     }
 
-    fun size(): Int {
+    /**
+     * 实际存储的元素个数
+     */
+    fun count(): Int {
         return tail - head
+    }
+
+    fun printElement() {
+        val strBuilder = StringBuilder()
+        items.forEachIndexed { index, s ->
+            strBuilder.append("$index:$s,")
+        }
+        println(strBuilder.toString())
     }
 
 }
